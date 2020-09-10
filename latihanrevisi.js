82142034fda44e68589df4484bfa43f5633b42af
Nugroho-00 <snugroho211@gmail.com>
2020-09-10 12:34:05 +0700

    revisi latihan backend dari kode sebelumnya

---
+++ latihanrevisi.js
@@ -0,0 +1,60 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/home", (request, response) => {
+  response.send("Welcome to my first backend!!!")
+});
+
+app.post("/data", (request, response) => {
+  console.log(request.body)
+  response.send('Oke');
+});
+
+app.post("/profile", (request, response) => {
+  response.send(request.body)
+});
+
+let data = {
+  nama: "jane",
+  batch: "17.2",
+  email: "janedoe@mail.com"
+};
+
+app.patch("/profile", (requset, response) => {
+  data = {
+    ...data, ...request.body
+  }
+  response.send(data)
+})
+
+app.put("/profile", (request, response) => {
+  const { name, batch, email } = request.body
+  if (name && batch && email) {
+    data = {
+      ...request.body
+    }
+    response.send(data)
+  } else {
+    response.send({
+      success: false,
+      message: "All form must be filled"
+    })
+  }
+})
+
+app.get("/profie", (request, response) => {
+  response.send(data)
+})
+
+app.delete("/profile", (request, response) => {
+  data = null,
+    response.send(data)
+})
+
+app.listen(3030, () => {
+  console.log("Server listening on port http://localhost:3030")
+})
